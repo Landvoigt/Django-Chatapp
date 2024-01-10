3fa59e68ff928e30076155ef477b83f046be88ab
Landvoigt <tim.voigt@ik.me>
2024-01-10 12:59:05 +0100

    chat, messages

--- chat/admin.py
+++ chat/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Message, Chat
 
-# Register your models here.
+class MessageAdmin(admin.ModelAdmin):
+    fields = ('chat','text', 'created_at','author','receiver')
+    list_display = ('author','receiver','text', 'created_at')
+    search_fields = ('text',)
+
+admin.site.register(Message, MessageAdmin)
+admin.site.register(Chat)
--- chat/views.py
+++ chat/views.py
@@ -1,4 +1,10 @@
 from django.shortcuts import render
+from chat.models import Message, Chat
 
 def index(request):
-    return render(request, 'chat/index.html')
+    if request.method == 'POST':
+        print("Received data" + request.POST['chatMessage'])
+        myChat = Chat.objects.get(id=1)
+        Message.objects.create(text=request.POST['chatMessage'], chat=myChat, author=request.user, receiver=request.user)
+    chatMessages = Message.objects.filter(chat__id=1)    
+    return render(request, 'chat/index.html', {'name': 'lv'}, {'messages':chatMessages},)
